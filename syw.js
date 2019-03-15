de9b97939e2d28e381bb67f3bc6c19186cb27404
Gregor Schneider <gregor@rheingold.websales.united.domain>
2019-03-15 15:07:31 +0100

    first commit

---
+++ syw.js
@@ -0,0 +1,32 @@
+var SYW = {
+	"data": [
+		{
+			"year": "1756",
+			"battles": ["Pirna","Lobositz"]
+		},
+		{
+			"year": "1757",
+			"battles": ["Prag","Kolin","Hastenbeck","Groß-Jägersdorf","Moys","Roßbach","Breslau","Leuthen"]
+		},
+		{
+			"year": "1758",
+			"battles": ["Rheinberg","Krefeld","Domstadtl","Olmütz","Mehr","Zorndorf","Saint-Cast","Hochkirch"]
+		},
+		{
+			"year": "1759",
+			"battles": ["Bergen","Kay","Minden","Kunersdorf","Lagos","Hoyerswerda","Bucht von Quiberon","Maxen"]
+		},
+		{
+			"year": "1760",
+			"battles": ["Koßdorf","Landeshut","Emsdorf","Warburg","Liegnitz","Berlin","Kloster Kampen","Torgau"]
+		},
+		{
+			"year": "1761",
+			"battles": ["Vellinghausen","Ölper"]
+		},
+		{
+			"year": "1762",
+			"battles": ["Döbeln","Burkersdorf","Reichenbach","Freiberg"]
+		}
+	]
+}
